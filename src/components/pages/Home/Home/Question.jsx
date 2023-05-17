4d3471b53bc73b395c54f68c919795dfedca4f14
PritamDutta <webdevpritam7498@gmail.com>
2023-05-17 22:25:08 +0600

    another section is added

--- src/components/pages/Home/Home/Question.jsx
+++ src/components/pages/Home/Home/Question.jsx
@@ -6,12 +6,13 @@ const Question = () => {
       <div className='p-5 md:p-10 py-10 md:py-16'>
         <p className=''>---- <span className='text-black tracking-widest'>Read cooking methods</span></p>
         <h2 className='text-1xl md:text-3xl'>
-          <span className='text-2xl md:text-3xl pl-1 md:pl-2 font-bold'>Want to cook something <span className='text-red-800'>testy</span>? Read our <span className='text-red-800'>best recipes</span> </span>
+          <span className='text-2xl md:text-3xl pl-1 md:pl-2 font-bold'>Want to cook something <span className='text-orange-400'>testy</span>? Read our <span className='text-orange-500'>best recipes</span> </span>
         </h2>
-        <button className="btn btn-outline btn-secondary">Recipes</button>
+        <button className="btn btn-outline btn-secondary" >Recipes</button>
+       
       </div>
       <div className='bg-pink-100 p-10 rounded-xl text-center'>
-        <p className=''>---- <span className='text-red-800 tracking-widest'>Talk with chefs</span></p>
+        <p className=''> <span className='text-red-800 tracking-widest'>Talk with chefs</span></p>
         <div className='py-3'>
           <hr className='bg-red-800' />
         </div>
@@ -29,7 +30,7 @@ const Question = () => {
               <span className='text-3xl text-red-800'>19:00</span>
             </div>
           </div>
-            <p className='w-2/3 md:w-1/2 mx-auto border border-red-900 p-2 my-2 rounded-lg'>+8801860239857</p>
+            <p className='w-2/3 md:w-1/2 mx-auto border border-red-900 p-2 my-2 rounded-lg'>We serve to our best food to you</p>
         </h2>
       </div>
     </div>
